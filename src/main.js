c9322146c42a80280a0791b12959ad5b392a31b8
kimmvb <kimveb1@gmail.com>
2023-08-03 14:58:32 -0500

    holaaa

--- src/main.js
+++ src/main.js
@@ -9,18 +9,18 @@ const filter = document.getElementById("button-filter");
 createFilms(data);
 
 enterButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    sectionHome.classList.remove("show");
-    sectionHome.classList.add("hidden");
-    sectionMovies.classList.remove("hidden");
-    sectionMovies.classList.add("show");
-    document.body.classList.remove("body-blue");
+  event.preventDefault();
+  sectionHome.classList.remove("show");
+  sectionHome.classList.add("hidden");
+  sectionMovies.classList.remove("hidden");
+  sectionMovies.classList.add("show");
+  document.body.classList.remove("body-blue");
 });
 
 function createFilms(data) {
-    root.innerHTML = "";
-    for (let i = 0; i < data.films.length; i++) {
-        root.innerHTML += `<figure class="poster">
+  root.innerHTML = "";
+  for (let i = 0; i < data.films.length; i++) {
+    root.innerHTML += `<figure class="poster">
         <div class="info">
             <p class="title-tooltip"><b>${data.films[i].title}</b></p> 
             <p><b>Rating<b/>: ⭐${data.films[i].rt_score} / <b>Año:</b> ${data.films[i].release_date}</p> 
@@ -32,17 +32,17 @@ function createFilms(data) {
         <img src="${data.films[i].poster}" alt="${data.films[i].title}" />
         <figcaption>${data.films[i].title}</figcaption> 
         </figure>`;
-    }
+  }
 }
 const dataGhibli = data.films;
 
 const charactersRoot = document.getElementById("characters-small-container");
 
 function printDataCharacters(array) {
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array[i].people.length; j++) {
-            //console.log(array[i].people.length);
-            charactersRoot.innerHTML += `<div class="characters-each">
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].people.length; j++) {
+      //console.log(array[i].people.length);
+      charactersRoot.innerHTML += `<div class="characters-each">
          <img src="${array[i].people[j].img}" class="poster-ch">
          <h3>${array[i].people[j].name}</h3> 
          <hr>
@@ -50,56 +50,56 @@ function printDataCharacters(array) {
          <p><strong>Specie</strong>: ${array[i].people[j].specie}</p>
          <p><strong>Gender</strong>: ${array[i].people[j].gender}</p>
         </div>`;
-        }
     }
+  }
 }
 printDataCharacters(dataGhibli);
 
 //función alfabéticamente ascendente
 function aToZ(array) {
-    const titles = [];
-    for (let i = 0; i < array.length; i++) {
-        titles.push(array[i].title);
-    }
-    console.log(titles.sort());
+  const titles = [];
+  for (let i = 0; i < array.length; i++) {
+    titles.push(array[i].title);
+  }
+  console.log(titles.sort());
 }
 aToZ(dataGhibli);
 
 //función alfabéticamente descendente
 function zToa(array) {
-    const titles = [];
-    for (let i = 0; i < array.length; i++) {
-        titles.push(array[i].title);
-    }
-    console.log(titles.reverse());
+  const titles = [];
+  for (let i = 0; i < array.length; i++) {
+    titles.push(array[i].title);
+  }
+  console.log(titles.reverse());
 }
 zToa(dataGhibli);
 
 //función años descendente
 function yearsDown(array) {
-    const yearsMovies = [];
-    for (let i = 0; i < array.length; i++) {
-        yearsMovies.push(array[i].release_date);
-    }
-    console.log(
-        yearsMovies.sort(function (a, b) {
-            return a - b;
-        }),
-    );
+  const yearsMovies = [];
+  for (let i = 0; i < array.length; i++) {
+    yearsMovies.push(array[i].release_date);
+  }
+  console.log(
+    yearsMovies.sort(function (a, b) {
+      return a - b;
+    }),
+  );
 }
 yearsDown(dataGhibli);
 
 //función años ascendente
 function yearsUp(array) {
-    const yearsMovies = [];
-    for (let i = 0; i < array.length; i++) {
-        yearsMovies.push(array[i].release_date);
-    }
-    console.log(
-        yearsMovies.sort(function (a, b) {
-            return b - a;
-        }),
-    );
+  const yearsMovies = [];
+  for (let i = 0; i < array.length; i++) {
+    yearsMovies.push(array[i].release_date);
+  }
+  console.log(
+    yearsMovies.sort(function (a, b) {
+      return b - a;
+    }),
+  );
 }
 yearsUp(dataGhibli);
 
