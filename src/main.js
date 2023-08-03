518baca3a58a264effce3ac0748e3230825e1c3f
karla aqueveque <aquevequekarla@gmail.com>
2023-08-03 13:30:04 -0400

    section vehiculos

--- src/main.js
+++ src/main.js
@@ -58,3 +58,68 @@ function printDataCharacters(array) {
     }
 }
 printDataCharacters(dataGhibli);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function printDataVehicles(data) {
+//     root.innerHTML = '';
+//     for (let i = 0; i < data.films.length; i++) {
+//         root.innerHTML += `<figure class="poster">
+//         <div class="info">
+//             <p class="title-tooltip"><b>${data.films[i].title}</b></p>
+//             <p><b>Rating<b/>: ⭐${data.films[i].rt_score} / <b>Año:</b> ${data.films[i].release_date}</p>
+//             <p>${data.films[i].description}</p>
+//             <br>
+//             <p>Director: <b>${data.films[i].director}</b></p>
+//             <p>Productor: <b>${data.films[i].producer}</b></p>
+//         </div>
+//         <img src="${data.films[i].poster}" alt="${data.films[i].title}" />
+//         <figcaption>${data.films[i].title}</figcaption>
+//         </figure>`;
+//     }
+// }
