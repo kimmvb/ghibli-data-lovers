fe5b1c58d590e6f8ff91524ad2323330ea430922
kimmvb <kimveb1@gmail.com>
2023-08-05 00:27:31 -0500

    Función para mostrar y ocultar pestañas

--- src/data.js
+++ src/data.js
@@ -1,13 +1,16 @@
-// métodos
-
-export const aToZ = (data) => {
-
-  return data.sort();
+export const showTab = (tabName, tabContents) => {
+  tabContents.forEach(function(tab) {
+    tab.style.display = 'none';
+  });
+  const selectedTab = document.getElementById(tabName);
+  selectedTab.style.display = 'block'
 };
 
-export const anotherExample = () => {
+// Se crea la función showTab cuyos parámetros son tabName (nombre de la pestaña) y tabContent (contenido de la pestaña)
+//Con la función forEach se itera por cada elemento del contenido de las pestañas, la acción que sucederá por la iteración es que por cada pestaña se ocultará, con un style.display.
+//Luego se crea una variable que contiene un DOM que llama al nombre de la pestaña
+//A la pestaña seleccionada se le cambiará el display a 'block' para mostrarlo
+
+/*export const anotherExample = () => {
   return 'OMG';
-};
-
- 
-
+};*/
